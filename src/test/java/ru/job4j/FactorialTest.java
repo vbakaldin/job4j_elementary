c5950f62252336594a3fc6908f55132e99e76ba0
vladimir <tintutin@list.ru>
2026-03-11 18:51:16 +0400

    factorial

---
+++ src/test/java/ru/job4j/FactorialTest.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FactorialTest {
+    @Test
+    void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int expected = 120;
+        int number = 5;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalculateFactorialForZeroThenOne() {
+        int expected = 1;
+        int number = 0;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+}

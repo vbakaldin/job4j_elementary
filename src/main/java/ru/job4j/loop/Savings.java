bdc6212a1ccfa0559ca25e908189d940b3c6526e
vladimir <tintutin@list.ru>
2026-03-11 21:12:52 +0400

    Savings

---
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Savings {
+    public static int years(double goal, double annualDeposit, double percent) {
+        int years = 0;
+        double total = 0;
+        do {
+            total = total + annualDeposit + total * percent / 100;
+            years++;
+        } while (total <= goal);
+        return years;
+    }
+}

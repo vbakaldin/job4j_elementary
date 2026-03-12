b13caa74aa2d0b90f5754a6a2a57330d90fb831f
vladimir <tintutin@list.ru>
2026-03-12 16:24:14 +0400

    ArrayLoop foreach

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -8,8 +8,8 @@ public class ArrayLoop {
             numbers[i] = i * 2 + 3;
         }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(numbers[i]);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }

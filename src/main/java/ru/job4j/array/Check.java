a8fbd4853192128d13e2393dd69d73c9dd52ba58
vladimir <tintutin@list.ru>
2026-03-12 19:00:00 +0400

    Check

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class Check {
+
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 0; i < data.length - 1; i++) {
--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -3,6 +3,7 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 class CheckTest {
     @Test
     void whenDataMonoByTrueThenTrue() {

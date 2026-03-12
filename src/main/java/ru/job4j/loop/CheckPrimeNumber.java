10789df59c37aab90757c7bee7bd47f5631afba4
vladimir <tintutin@list.ru>
2026-03-12 11:45:10 +0400

    CheckPrimeNumber

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,15 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean primeNumber = true;
+        boolean primeNumber = number > 1;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 primeNumber = false;
                 break;
             }
-        }
-        if (number < 2) {
-            primeNumber = false;
         }
         return primeNumber;
     }

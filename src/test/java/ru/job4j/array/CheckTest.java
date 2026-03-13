7f867e641c988a2e3156e84d03c22d2902004594
vladimir <tintutin@list.ru>
2026-03-13 16:01:46 +0400

    some error

--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 class CheckTest {
     @Test
     void whenDataMonoByTrueThenTrue() {

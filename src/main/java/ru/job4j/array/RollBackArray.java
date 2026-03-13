a33a609ecd2cba2650fc2b5cbf822a0dc8d9c1f1
vladimir <tintutin@list.ru>
2026-03-13 15:38:57 +0400

    RollBackArray

--- src/main/java/ru/job4j/array/RollBackArray.java
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -4,7 +4,7 @@ public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int index = 0; index < array.length; index++) {
-            result[index] = array[ array.length - index - 1 ];
+            result[index] = array[array.length - index - 1];
         }
         return result;
     }
